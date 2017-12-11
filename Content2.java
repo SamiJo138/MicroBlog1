e3e5ba2be78de2dc0b237ecbc36d55204a4e92f7
Sami Jo Cook <Samijo138@gmail.com>
2017-12-11 12:09:21 -0600

    Update Content2.java

--- Content2.java
+++ Content2.java
@@ -1,16 +1,42 @@
+package Microblog;
+import java.util.*;
+
 
 class Content2 {
 
-    private String content;
-    private String userPost;
+    private static int nextPostNum = 1;
+    private int postNum;
+    private String newPost;
+    private String currentUser;
+    private String website;
 
-public String getContent()
-        {
-            return content + "\n" +userPost;
-        }
-        Content2(String postContent, String userPosted)
-        {
-            this.content=postContent;
-            this.userPost=userPosted;
-        }
+    Content2(int postNum,String newPost, String currentUser, String website) {
+
+        this.postNum= postNum;
+        this.newPost = newPost;
+        this.currentUser = currentUser;
+        this.website = website;
+    }
+
+    public void getContent() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter current user's username");
+        this.currentUser = keyboard.next();
+        System.out.println("Post:");
+        this.newPost = keyboard.next();
+    }
+
+
+    public void printContent() {
+        System.out.println(this.postNum + " " + this.currentUser);
+        System.out.println(this.newPost);
+    }
+
+    public String toString() {
+        return this.postNum + " " + this.currentUser + "/n" + this.newPost;
+
+    }
+    public String getUser(){
+        return this.currentUser;
+    }
 }
