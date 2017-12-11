369fa534a765c0cac6aa0c8ce0f355c2d998ed9c
Sami Jo Cook <Samijo138@gmail.com>
2017-12-11 12:04:58 -0600

    Update Post.java

--- Post.java
+++ Post.java
@@ -1,24 +1,23 @@
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Post
 {
     public String user;
     public String postNum;
     public String posts;
-    public String post;
     public String website;
- public void Post(String user,String postNum, String posts, String website)
+
+    Post(String user, String postNum, String posts, String website)
     {
         this.user=user;
         this.postNum=postNum;
         this.posts=posts;
-        this.post=post;
         this.website=website;
     }
-  
-           public String getPostString()
-    { 
-        return this.user+"  " +this.posts+ " | "+this.website;
+
+    public String getPostString()
+    {
+        return user+"  " +postNum+ "  "+posts+ " | "+website;
     }
+
+
 }
